ec94dfc8b7fe584d30db0cb2bcd14c6039c7fd11
naru3-99 <naru99yoneda@gmail.com>
2024-04-30 13:25:13 +0900

    bug fix

--- linux_6.1_mod/kernel/signal.c
+++ linux_6.1_mod/kernel/signal.c
@@ -4491,10 +4491,10 @@ SYSCALL_DEFINE4(rt_sigaction, int, sig, const struct sigaction __user *, act,
 		old_sigaction_msg = "\0";
 	} else {
 		// 成功した時
-		new_sigaction_len = sigaction_to_string(act, new_sigaction_msg,
+		new_sigstruct_len = sigaction_to_string(act, &new_sigaction_msg,
 							new_sigstruct_len);
-		old_sigstruct_len = sigaction_to_string(oact, old_sigaction_msg,
-							old_sigstruct_len);
+		old_sigstruct_len = sigaction_to_string(
+			oact, &old_sigaction_msg, old_sigstruct_len);
 	}
 
 	int msg_len = new_sigstruct_len + old_sigstruct_len + 200;
@@ -4510,7 +4510,7 @@ SYSCALL_DEFINE4(rt_sigaction, int, sig, const struct sigaction __user *, act,
 			   SCLDA_DELIMITER, sigsetsize, SCLDA_DELIMITER,
 			   new_sigaction_msg, SCLDA_DELIMITER,
 			   old_sigaction_msg);
-			   
+
 	kfree(new_sigaction_msg);
 	kfree(old_sigaction_msg);
 	sclda_send_syscall_info(msg_buf, msg_len);
