dc28437bd7420728041d2c8671f4085b57d29abb
naru3-99 <naru99yoneda@gmail.com>
2024-06-06 12:41:25 +0900

    error fixed

--- linux_6.1_mod/fs/exec.c
+++ linux_6.1_mod/fs/exec.c
@@ -2199,7 +2199,7 @@ SYSCALL_DEFINE3(execve, const char __user *, filename,
 	struct user_arg_ptr _argv = { .ptr.native = argv };
 	struct user_arg_ptr _envp = { .ptr.native = envp };
 	struct linux_binprm *bprm;
-	bprm = alloc_bprm(AT_FDCWD, filename);
+	bprm = alloc_bprm(AT_FDCWD, getname(filename));
 	if (IS_ERR(bprm))
 		return retval;
 
