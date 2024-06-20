6ce8101624122c076d1d345afeabe3d95ee85dad
naru3-99 <naru99yoneda@gmail.com>
2024-06-20 18:48:26 +0900

    86: linkの改造

--- linux_6.1_mod/fs/namei.c
+++ linux_6.1_mod/fs/namei.c
@@ -4733,8 +4733,49 @@ SYSCALL_DEFINE5(linkat, int, olddfd, const char __user *, oldname, int, newdfd,
 SYSCALL_DEFINE2(link, const char __user *, oldname, const char __user *,
 		newname)
 {
-	return do_linkat(AT_FDCWD, getname(oldname), AT_FDCWD, getname(newname),
-			 0);
+	int retval;
+	int msg_len, old_len, new_len;
+	char *msg_buf, *old_buf, *new_buf;
+
+	retval = do_linkat(AT_FDCWD, getname(oldname), AT_FDCWD,
+			   getname(newname), 0);
+	if (!is_sclda_allsend_fin())
+		return retval;
+
+	// oldnameを取得する
+	old_len = strnlen_user(oldname, old_MAX);
+	old_buf = kmalloc(old_len + 1, GFP_KERNEL);
+	if (!old_buf)
+		return retval;
+	if (copy_from_user(old_buf, oldname, old_len))
+		goto free_old;
+	old_buf[old_len] = '\0';
+
+	// newnameを取得する
+	new_len = strnlen_user(newname, new_MAX);
+	new_buf = kmalloc(new_len + 1, GFP_KERNEL);
+	if (!new_buf)
+		goto free_old;
+	if (copy_from_user(new_buf, newname, new_len))
+		goto free_new;
+	new_buf[new_len] = '\0';
+
+	// 送信するパート
+	msg_len = 100 + old_len;
+	msg_buf = kmalloc(msg_len, GFP_KERNEL);
+	if (!msg_buf)
+		goto free_new;
+
+	msg_len = snprintf(msg_buf, msg_len, "86%c%d%c%s%c%s", SCLDA_DELIMITER,
+			   retval, SCLDA_DELIMITER, old_buf, SCLDA_DELIMITER,
+			   new_buf);
+	sclda_send_syscall_info(msg_buf, msg_len);
+
+free_new:
+	kfree(new_buf);
+free_old:
+	kfree(old_buf);
+	return retval;
 }
 
 /**
