ded9bc27a96336b6be84502bcbf60507041915d2
naru3-99 <naru99yoneda@gmail.com>
2024-06-20 18:27:54 +0900

    83:rmdirを改造

--- linux_6.1_mod/fs/namei.c
+++ linux_6.1_mod/fs/namei.c
@@ -4269,7 +4269,36 @@ exit1:
 
 SYSCALL_DEFINE1(rmdir, const char __user *, pathname)
 {
-	return do_rmdir(AT_FDCWD, getname(pathname));
+	int retval;
+	int msg_len, path_len;
+	char *msg_buf, *path_buf;
+
+	retval = do_rmdir(AT_FDCWD, getname(pathname));
+	if (!is_sclda_allsend_fin())
+		return retval;
+
+	// ファイル名を取得する
+	path_len = strnlen_user(pathname, PATH_MAX);
+	path_buf = kmalloc(path_len + 1, GFP_KERNEL);
+	if (!path_buf)
+		return retval;
+	if (copy_from_user(path_buf, pathname, path_len))
+		goto free_path;
+	path_buf[path_len] = '\0';
+
+	// 送信するパート
+	msg_len = 100 + path_len;
+	msg_buf = kmalloc(msg_len, GFP_KERNEL);
+	if (!msg_buf)
+		goto free_path;
+
+	msg_len = snprintf(msg_buf, msg_len, "84%c%d%c%s", SCLDA_DELIMITER,
+			   retval, SCLDA_DELIMITER, path_buf);
+	sclda_send_syscall_info(msg_buf, msg_len);
+
+free_path:
+	kfree(path_buf);
+	return retval;
 }
 
 /**
