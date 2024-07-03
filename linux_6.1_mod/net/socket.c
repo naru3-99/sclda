04caaf0f184e3daafecbfc23b045503d7a50fb9c
naru3-99 <naru99yoneda@gmail.com>
2024-07-03 15:30:19 +0900

    リファクタリング

--- linux_6.1_mod/net/socket.c
+++ linux_6.1_mod/net/socket.c
@@ -133,7 +133,7 @@ int sockaddr_to_str(struct sockaddr_storage *ss, char *buf, int len)
 		addr_in = (struct sockaddr_in *)ss;
 		ip = ntohl(addr_in->sin_addr.s_addr);
 		snprintf(info_buf, info_len,
-			 "ip= %u:%u:%u:%u"
+			 "ipv4: address= %u:%u:%u:%u"
 			 " port= %u",
 			 (ip >> 24) & 0xFF, (ip >> 16) & 0xFF, (ip >> 8) & 0xFF,
 			 ip & 0xFF, (unsigned int)ntohs(addr_in->sin_port));
@@ -145,7 +145,7 @@ int sockaddr_to_str(struct sockaddr_storage *ss, char *buf, int len)
 
 		// ipアドレスを取得する
 		addr_in6 = (struct sockaddr_in6 *)ss;
-		offset = snprintf(info_buf, info_len, "ip= %02x",
+		offset = snprintf(info_buf, info_len, "ipv6: address= %02x",
 				  addr_in6->sin6_addr.in6_u.u6_addr8[0]);
 		for (i = 1; i < 16; i++) {
 			offset += snprintf(
