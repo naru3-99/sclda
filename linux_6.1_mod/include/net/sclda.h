1d2984d3a318ac922ee18b352f909ba49955c3af
naru3-99 <naru99yoneda@gmail.com>
2024-08-09 22:18:30 +0900

    マジックナンバーの更新

--- linux_6.1_mod/include/net/sclda.h
+++ linux_6.1_mod/include/net/sclda.h
@@ -55,7 +55,7 @@
 // bufsize for utime, PID
 #define SCLDA_PID_CLOCK_SIZE ((int)50)
 // bufsize for syscall_info struct
-#define SCLDA_NUM_TO_SEND_SINFO ((int)4096)
+#define SCLDA_NUM_TO_SEND_SINFO ((int)8192)
 // num for head of syscall_info struct
 #define SCLDA_SCI_NUM ((int)8)
 // max size of the buffer
