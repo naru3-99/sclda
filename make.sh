31dd6e28edb62fb0296915419ffa916af6ea77e3
naru3-99 <naru99yoneda@gmail.com>
2024-09-06 17:34:41 +0900

    commentout: cp .config

--- make.sh
+++ make.sh
@@ -3,30 +3,30 @@ sudo apt install -y build-essential bc bison flex libelf-dev libssl-dev libncurs
 
 cd linux_6.1_mod
 
-cp /boot/config-$(uname -r) .config
+# cp /boot/config-$(uname -r) .config
 
-replace_string() {
-    search_string="$1"
-    replace_string="$2"
-    input_file="$3"
-    output_file="$4"
+# replace_string() {
+#     search_string="$1"
+#     replace_string="$2"
+#     input_file="$3"
+#     output_file="$4"
 
-    echo "" >> $output_file
+#     echo "" >> $output_file
 
-    while IFS='' read -r line || [[ -n "$line" ]]; do
-        if [[ $line == *"$search_string"* ]]; then
-            line="$replace_string"
-        fi
-        echo "$line" >> "$output_file"
-    done < "$input_file"
-}
+#     while IFS='' read -r line || [[ -n "$line" ]]; do
+#         if [[ $line == *"$search_string"* ]]; then
+#             line="$replace_string"
+#         fi
+#         echo "$line" >> "$output_file"
+#     done < "$input_file"
+# }
 
-replace_string "CONFIG_SYSTEM_TRUSTED_KEYS" 'CONFIG_SYSTEM_TRUSTED_KEYS=""' "./.config" "./temp1"
-replace_string "CONFIG_DEBUG_INFO_BTF" 'CONFIG_DEBUG_INFO_BTF=n' "./temp1" "./temp2"
+# replace_string "CONFIG_SYSTEM_TRUSTED_KEYS" 'CONFIG_SYSTEM_TRUSTED_KEYS=""' "./.config" "./temp1"
+# replace_string "CONFIG_DEBUG_INFO_BTF" 'CONFIG_DEBUG_INFO_BTF=n' "./temp1" "./temp2"
 
-rm -rf ./.config
-rm -rf ./temp1
-mv ./temp2 ./.config
+# rm -rf ./.config
+# rm -rf ./temp1
+# mv ./temp2 ./.config
 
 make localmodconfig
 echo start
